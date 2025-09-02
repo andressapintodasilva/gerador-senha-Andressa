9faa4a7c9442bca5c7af58c632bfbdab594e0c35
andressapintodasilva <146196237+andressapintodasilva@users.noreply.github.com>
2025-09-02 14:19:15 -0300

    2

--- main.js
+++ main.js
@@ -68,14 +68,17 @@ classificaSenha(alfabeto.length);
 function classificaSenha(tamanhoAlfabeto) {
    let entropia = tamanhoSenha*Math.log2(tamanhoSenha);
 
-
-
 forcaSenha.classList.remove('fraca', 'media', 'forte');
 if (entropia > 57) {
-forcaSenha.classList.add('forte');
+ forcaSenha.classList.add('forte');
 } else if (entropia > 35 && entropia < 57) {
-forcaSenha.classList.add('media');
+ forcaSenha.classList.add('media');
 } else if (entropia <= 35) {
-forcaSenha.classList.add('fraca');
+ forcaSenha.classList.add('fraca');
+
 }
+const valorEntropia = document.querySelector('.entropia');
+valorEntropia.textContent = Math.floor(2**entropia/(100e6*60*60*24));
+
+
 }
